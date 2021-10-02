e3cb0ee893bc9549c7a0c683fff6dd674c0a7d28
Aman <amanbhangre1@gmail.com>
2021-10-02 11:52:55 +0530

    Added name

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   return (
     <div className="App">
       <h1>React Django deploy on heroku</h1>
+      <p>This is aman bhangre</p>
       {
         blogs.map(blog => (
           <div className="blog" key={blog.id}>
